de54625fca12c904ecad8eeaf4d2a9042ab636f7
Gois <matheusgoislimasilva@gmail.com>
2020-06-01 14:54:00 -0300

    Initial Commit

---
+++ infinit_list/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  infinit_list
+//
+//  Created by Matheus Silva on 01/06/20.
+//  Copyright © 2020 Matheus Gois. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
